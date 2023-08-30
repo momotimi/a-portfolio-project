c4d8714184eb7e0febc06c9577c0ffe63df1a2fa
momotimi <mike.momotimi@gmail.com>
2023-08-30 03:57:07 +0100

    fisrt commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./build/**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        herosection_color: "#697861",
+        herosectionlight_color: "#ff78e5"
+      }
+    },
+  },
+  plugins: [],
+}
+
